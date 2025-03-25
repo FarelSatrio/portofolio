4689f5d5bd72c5d0304717d30841673166dc892b
FarelSatrio <farelsatriop@gmail.com>
2025-03-25 13:06:01 +0700

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed(".typedtext", {
+strings: ["Cloud Computing Enthusiast", "AWS Learner", "Future DevOps Engineer"],
+typeSpeed: 60,
+backSpeed: 60,
+loop: true,
+showCursor: true
+});
+
