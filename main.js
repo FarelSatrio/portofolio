30976863ed5499e87555c594d9b563c6e086a901
FarelSatrio <farelsatriop@gmail.com>
2025-03-25 13:36:42 +0700

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var typed = new Typed(".typedtext", {
-strings: ["Cloud Computing Enthusiast", "AWS Learner", "Future DevOps Engineer"],
-typeSpeed: 60,
+strings: ["Student", "AWS Learner"],
+typeSpeed: 70,
 backSpeed: 60,
 loop: true,
 showCursor: true
